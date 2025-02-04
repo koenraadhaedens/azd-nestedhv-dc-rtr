d889ae1df57c1b8e22018c02dd1b15c10c5f9a7f
Koenraad Haedens <koenraad@khd.be>
2025-02-04 16:21:04 +0100

    add download iso file to ps1

--- infra/customscript.ps1
+++ infra/customscript.ps1
@@ -1,4 +1,9 @@
 
+# download windows server 2022 eval version iso
+$downloadUrl = "https://go.microsoft.com/fwlink/p/?LinkID=2195280&clcid=0x409&culture=en-us&country=US"
+$outputPath = "C:\Your\Desired\Folder\WindowsServer2022.iso"
+Invoke-WebRequest -Uri $downloadUrl -OutFile $outputPath
+
 
 # Install Hyper-V feature
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -restart
