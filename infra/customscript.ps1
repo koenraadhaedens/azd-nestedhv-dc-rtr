496ca0a3fee126cc6fe7af8842f94e38e56a2a03
Koenraad Haedens <koenraad@khd.be>
2025-02-04 16:29:43 +0100

    add stuff to ps1

--- infra/customscript.ps1
+++ infra/customscript.ps1
@@ -1,9 +1,24 @@
+﻿Start-Transcript "C:\BootstrapHost_log.txt"
+$ErrorActionPreference = 'SilentlyContinue'
+
+# Create path OpsDir
+$opsDir = "C:\OpsDir"
+New-Item -Path $opsDir -ItemType directory -Force
+
+# Create path iso
+$iso = "C:\iso"
+New-Item -Path $iso -ItemType directory -Force
+
 
 # download windows server 2022 eval version iso
 $downloadUrl = "https://go.microsoft.com/fwlink/p/?LinkID=2195280&clcid=0x409&culture=en-us&country=US"
-$outputPath = "C:\Your\Desired\Folder\WindowsServer2022.iso"
+$outputPath = Join-Path -Path $iso -ChildPath "WindowsServer2022.iso"
+
 Invoke-WebRequest -Uri $downloadUrl -OutFile $outputPath
 
 
+
 # Install Hyper-V feature
+Write-Output "Install Hyper-V and restart"
+Stop-Transcript
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -restart
