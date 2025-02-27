3804112de195fa19c5b6bceb9ba8059d8f573bc5
Koenraad Haedens <40601026+koenraadhaedens@users.noreply.github.com>
2025-02-27 09:28:17 +0100

    Update postprovision.ps1

--- postprovision.ps1
+++ postprovision.ps1
@@ -1,11 +1,12 @@
 azd env get-values > .env
-
+$environmentName = $env:AZURE_ENV_NAME
 # sending stats to table please comment out if you do not want this
 
 $webhookUrl = "https://8116ebc5-9750-4a45-bb68-3623eef692f3.webhook.ne.azure-automation.net/webhooks?token=ZEwDwUSa225CZVgKPQ7ZDDe6K%2f8k9sMl2ou1FJlYpMA%3d"
 
 $deploymentData = @{
     Deployment = "azd-nestedhv-dc-rtr"
+    environmentName = $environmentName
     Machine = $env:AZUREPS_HOST_ENVIRONMENT
     CommitHash = (git rev-parse HEAD)
   } | ConvertTo-Json -Depth 10
