5eeb0d6789bbf2e137386f6f19122ce9dfd4fe02
koenraad@khd.be <koenraad@khd.be>
2025-03-08 15:09:18 +0100

    comment adjust

--- scripts/postprovision.sh
+++ scripts/postprovision.sh
@@ -7,7 +7,7 @@ azd env get-values > .env
 environmentName=$AZURE_ENV_NAME
 location=$AZURE_LOCATION
 
-# Uncomment the lines below if you want to send stats to a table
+# comment the lines below if you do not want to send stats to a table
  webhookUrl="https://8116ebc5-9750-4a45-bb68-3623eef692f3.webhook.ne.azure-automation.net/webhooks?token=ZEwDwUSa225CZVgKPQ7ZDDe6K%2f8k9sMl2ou1FJlYpMA%3d"
  commitHash=$(git rev-parse HEAD)
  deploymentData=$(jq -n --arg deployment "azd-nestedhv-dc-rtr" --arg location "$location" --arg environmentName "$environmentName" --arg machine "$AZUREPS_HOST_ENVIRONMENT" --arg commitHash "$commitHash" '{Deployment: $deployment, location: $location, environmentName: $environmentName, Machine: $machine, CommitHash: $commitHash}')
