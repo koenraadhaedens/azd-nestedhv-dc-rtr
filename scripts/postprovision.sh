154db801f628637f9b641e134254d9b7098a812a
Koenraad Haedens <40601026+koenraadhaedens@users.noreply.github.com>
2026-04-21 08:09:08 +0200

    Update webhook URL in postprovision script

--- scripts/postprovision.sh
+++ scripts/postprovision.sh
@@ -8,7 +8,7 @@ environmentName=$AZURE_ENV_NAME
 location=$AZURE_LOCATION
 
 # comment the lines below if you do not want to send stats to a table
- webhookUrl="https://8116ebc5-9750-4a45-bb68-3623eef692f3.webhook.ne.azure-automation.net/webhooks?token=ZEwDwUSa225CZVgKPQ7ZDDe6K%2f8k9sMl2ou1FJlYpMA%3d"
+ webhookUrl="https://8116ebc5-9750-4a45-bb68-3623eef692f3.webhook.ne.azure-automation.net/webhooks?token=mkbmnnnhgDsL20iey6qRj9Vc0ylCVg%2bpeZ1Yym7rsZs%3d"
  commitHash=$(git rev-parse HEAD)
  deploymentData=$(jq -n --arg deployment "azd-nestedhv-dc-rtr" --arg location "$location" --arg environmentName "$environmentName" --arg machine "$AZUREPS_HOST_ENVIRONMENT" --arg commitHash "$commitHash" '{Deployment: $deployment, location: $location, environmentName: $environmentName, Machine: $machine, CommitHash: $commitHash}')
  curl -X POST -H "Content-Type: application/json" -d "$deploymentData" "$webhookUrl"
