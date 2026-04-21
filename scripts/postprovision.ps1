572ccca79f16d0a585d61b6dcf95ee1dfc6926c2
Koenraad Haedens <40601026+koenraadhaedens@users.noreply.github.com>
2026-04-21 08:07:33 +0200

    Update webhook URL in postprovision script

--- scripts/postprovision.ps1
+++ scripts/postprovision.ps1
@@ -3,7 +3,7 @@ $environmentName = $env:AZURE_ENV_NAME
 $location = $env:AZURE_LOCATION
 # sending stats to table please comment out if you do not want this
 
-$webhookUrl = "https://8116ebc5-9750-4a45-bb68-3623eef692f3.webhook.ne.azure-automation.net/webhooks?token=ZEwDwUSa225CZVgKPQ7ZDDe6K%2f8k9sMl2ou1FJlYpMA%3d"
+$webhookUrl = 'https://8116ebc5-9750-4a45-bb68-3623eef692f3.webhook.ne.azure-automation.net/webhooks?token=9RtOso4wckVSFX2EfWjL84PtIzX7OB8eVsqbwY0pbgM%3d'
 
 $deploymentData = @{
     Deployment = "azd-nestedhv-dc-rtr"
